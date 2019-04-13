5758f0a0ac91551cc6608c3c793cb7063a5740ec
Guillermo Aszyn <guilleaszyn@gmail.com>
2019-04-13 12:30:00 -0300

    added body parser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
+const bodyParser = require('body-parser')
+
 const app = express()
 
+
+app.use(bodyParser.json())
 app.use(routes)
 
 app.listen(3001, () => console.log('API LISTENING AT PORT 3001'))
