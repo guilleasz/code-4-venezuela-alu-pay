d087061cb7b42dfc18f2b41a36b12e186c0f0f23
lucascurti <lucas.curti@gmail.com>
2019-04-13 16:38:02 -0300

    added login flow

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
-
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+const options = {
+  key: fs.readFileSync("./client-key.pem"),
+  cert: fs.readFileSync("./client-cert.pem")
+};
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(routes);
 
-app.listen(3001, () => console.log("API LISTENING AT PORT 3001"));
+// Create an HTTP service.
+http
+  .createServer(app)
+  .listen(3001, () => console.log("HTTP API LISTENING AT PORT 3001"));
+// Create an HTTPS service identical to the HTTP service.
+https
+  .createServer(options, app)
+  .listen(3002, () => console.log("HTTPS API LISTENING AT PORT 3002"));
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,44 @@ const express = require("express");
 const router = express.Router();
 const transactionsRoutes = require("./transactions");
 const axios = require("axios");
+const qs = require("qs");
+
+const UPHOLD_LOGIN_URL =
+  "https://sandbox.uphold.com/authorize/960e78e5c052c35f465f60864fe0db42f01bd08c?scope=accounts:read%20cards:read%20cards:write%20contacts:read%20contacts:write%20phones:read%20phones:write%20transactions:deposit%20transactions:read%20transactions:transfer:application%20transactions:transfer:others%20transactions:transfer:self%20transactions:withdraw%20transactions:commit:otp%20user:read";
+const UPHOLD_STATE = "sdf234khj234o23498hsdf923";
+const CLIENT_ID = "960e78e5c052c35f465f60864fe0db42f01bd08c";
+const CLIENT_SECRET = "b55da43669a74c67955153a2ec5dc480098e7426";
 
 router.get("/login", (req, res) => {
-  res.redirect(
-    "https://sandbox.uphold.com/authorize/960e78e5c052c35f465f60864fe0db42f01bd08c?scope=accounts:read%20cards:read%20cards:write%20contacts:read%20contacts:write%20phones:read%20phones:write%20transactions:deposit%20transactions:read%20transactions:transfer:application%20transactions:transfer:others%20transactions:transfer:self%20transactions:withdraw%20transactions:commit:otp%20user:read&state=sdf234khj234o23498hsdf923"
-  );
+  res.redirect(`${UPHOLD_LOGIN_URL}&state=${UPHOLD_STATE}`);
 });
-router.get("/uphold/callback", (req, res) => {});
+router.get("/uphold/callback", async (req, res) => {
+  console.log(req.query);
+  const { code, state } = req.query;
+
+  const response = await axios
+    .request({
+      url: "https://api-sandbox.uphold.com/oauth2/token",
+      method: "post",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: qs.stringify({
+        code: code,
+        grant_type: "authorization_code"
+      }),
+      auth: {
+        username: CLIENT_ID,
+        password: CLIENT_SECRET
+      }
+    })
+    .catch(error => error);
+
+  const credentials = response.data;
+  if (credentials && credentials.access_token) {
+    res.json(credentials);
+  } else {
+    res.json({ error: true, errorMessage: "Error getting access-token" });
+  }
+});
 
 router.get("/", (req, res) => res.send("Hellow World!"));
 router.use("/transactions", transactionsRoutes);
