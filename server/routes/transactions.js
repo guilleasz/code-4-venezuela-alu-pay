230f205ac264b2cf7e5e9f064786f2709b90ab04
Guillermo Aszyn <guilleaszyn@gmail.com>
2019-04-13 16:07:37 -0300

    added validation to transactions

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -3,6 +3,13 @@ const axios = require('axios')
 
 
 router.post('/', (req, res) => {
+  if(
+    !req.body.destinations.length ||
+    !req.body.cardId ||
+    !req.body.amount
+  ) {
+    return res.status(403).send('Required params missing')
+  }
   const transactions  = req.body.destinations.map(destination => 
     axios.post(`/me/cards/${req.body.cardId}/transactions?commit=true`,{
       denomination: {
