86fded03afbfa5b183fc3d28053af4b160ac891e
Guillermo Aszyn <guilleaszyn@gmail.com>
2019-04-14 14:25:24 -0300

    connect contacts and cards with redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux'
 import AppRouter from "./AppRouter";
 import { authenticate } from "./redux/actions/authentication";
+
 class App extends Component {
 
   componentDidMount() {
--- src/redux/actions/cards.js
+++ src/redux/actions/cards.js
@@ -1,12 +1,12 @@
 import { SET_CARDS } from '../constants'
-import api from '../../config/api';
-import { dispatch } from 'rxjs/internal/observable/range';
+import uphold from '../../uphold-sdk';
+
 
 export const setCards = cards => ({
   type: SET_CARDS,
   cards
 }) 
 
-export const fetchCards = () =>
-  api.get('/cards')
-    .then(({ data }) => dispatch(setCards(data)))
+export const fetchCards = () => dispatch =>
+  uphold.getCards()
+    .then(({ items }) => dispatch(setCards(items)))
