eff8b1935508e455cc0de3c0529540ecf8b79b38
Juan Carrillo <carrillojuancarlos16@gmail.com>
2019-04-14 16:51:54 -0300

    add contact form

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -3,6 +3,7 @@ export const SET_CARDS = "SET_CARDS";
 
 // CONTACTS
 export const SET_CONTACTS = "SET_CONTACTS";
+export const CREATE_CONTACT = "CREATE_CONTACT";
 
 // LOGIN
 export const LOG_IN = "LOG_IN";
