daa51b51cea2ec409e6e89e336a6d910510d522e
Juan Carrillo <carrillojuancarlos16@gmail.com>
2019-04-14 11:16:09 -0300

    fix-header

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import AddContact from "./AddContact";
-import Header from "./Header";
 import List from "./List";
 import Empty from "./Empty";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 
-export { AddContact, Header, List, Empty, Login, PrivateRoute };
+export { AddContact, List, Empty, Login, PrivateRoute };
