6756a0c1c98c7cbc13c64c04e079673560115a6a
Guillermo Aszyn <guilleaszyn@gmail.com>
2019-04-14 11:22:48 -0300

    fixed package.json missing packages

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import AddContact from "./AddContact";
-import Header from "./Header";
 import List from "./List";
 import Empty from "./Empty";
 
-export { AddContact, Header, List, Empty };
+export { AddContact, List, Empty };
